3673412ea293825a507829b76ef94faed1974798
Frank Piva <francisco.j.piva@gmail.com>
2016-01-29 17:36:40 -0800

    housekeeping

--- app/views.py
+++ app/views.py
@@ -1,6 +1,31 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        },
+        {
+            'author': {'nickname': 'Frank'},
+            'body': 'What are you talking about?'
+            },
+        {
+            'author': {'nickname': 'Bruno'},
+            'body': 'I am the github scapegoat.'
+            }
+        ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
