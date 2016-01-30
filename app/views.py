221e9ea073f45ee788895042305873a3418fc21e
LucDoh <lucdhauthuille@hotmail.com>
2016-01-29 22:55:35 -0800

    testing logn

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
 from app import app
 
 @app.route('/')
@@ -37,9 +37,15 @@ def landing():
 def login2():
     return render_template("login2.html")
 
-@app.route('/login')
+@app.route('/login', methods =['GET', 'POST'])
 def login():
-    return render_template("login3.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] !='admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template("login3.html", error=error)
 
 @app.route('/register')
 def register():
