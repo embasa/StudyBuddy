490ab57063479f144c773b4630d8eb3fd6456983
Frank Piva <francisco.j.piva@gmail.com>
2016-01-29 19:51:12 -0800

    confirm

--- app/views.py
+++ app/views.py
@@ -21,7 +21,7 @@ def index():
             },
         {
             'author': {'nickname': 'Bruno'},
-            'body': 'I am the github scapegoat. foo a la bar confirm?'
+            'body': 'I am the github scapegoat. foo a la bar confirm???'
             }
         ]
     return render_template("index.html",
