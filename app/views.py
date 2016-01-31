cc277cdf2525582cc737abb496d364d0aae697e3
lucO <lucdoh@dohdoh.edu>
2016-01-31 04:53:48 -0800

    views.py: landing->landingV5

--- app/views.py
+++ app/views.py
@@ -72,7 +72,7 @@ def landing():
         name = session['username']
     else:
         return redirect(url_for('login'))
-    return render_template("landing.html",myName=name,listings=listings)
+    return render_template("landingV5.html",myName=name,listings=listings)
 
 @app.route('/login2')
 def login2():
